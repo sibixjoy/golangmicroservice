2480e3320a18cd3165cc0bb1c405d9b324dbe38f
sibixjoy <121486431+sibixjoy@users.noreply.github.com>
2023-02-06 17:24:44 +0530

    Add files via upload

--- micro/inventory.go
+++ micro/inventory.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 	"encoding/json"
 	"io/ioutil"
-
+	"strconv"
 	"github.com/gorilla/mux"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -37,6 +37,7 @@ func main () {
     router.HandleFunc("/inventory", inventory) .Methods("GET")
     router.HandleFunc("/addinventory",Addinventory) .Methods("POST")
     router.HandleFunc("/getinventory/{id}",Getinventory) .Methods("GET")
+	router.HandleFunc("/getinventoryqty/{id}/{Quantity}",GetinventoryQuantity) .Methods("GET")
     router.HandleFunc("/deleteinventory/{id}",Delinventory) .Methods("DELETE")
 
     http.ListenAndServe(":8003", router)
@@ -79,6 +80,7 @@ func Getinventory(w http.ResponseWriter, r *http.Request) {
 		
 }
 
+
 func Delinventory(w http.ResponseWriter, r *http.Request) {
 	var delinventory Inventory
 	inventoryID := mux.Vars(r)["id"]
@@ -87,4 +89,66 @@ func Delinventory(w http.ResponseWriter, r *http.Request) {
 			json.NewEncoder(w).Encode(delinventory)
 		
 }
+type Response struct {
+	Result  bool   `json:"result"`
+	Message string `json:"message"`
+}	
 
+
+func GetinventoryQuantity(w http.ResponseWriter, r *http.Request) {
+	var inventory Inventory
+	//var inventoryQty Inventory
+	//func getFieldInteger(e *Inventory, field string) int {
+		//r := reflect.ValueOf(e)
+		//f := reflect.Indirect(r).FieldByName(field)
+		//return int(f.Int())
+		
+	//}
+	inventoryqty := mux.Vars(r)["Quantity"]
+
+	qty, _ := strconv.Atoi(inventoryqty)
+
+
+	inventoryID := mux.Vars(r)["id"]
+
+	    db.First(&inventory, inventoryID) 
+        invent:=inventory 
+	fmt.Println("body is printing ", (invent))
+
+	//p:=invent
+
+		//if inventoryqty==(p.Quantity ){
+				//	fmt.Println("body is nyc")p:=inventory{}
+	p:=invent
+	
+	inty, _ := strconv.Atoi(inventoryID)
+
+	
+	if inty==(p.Id) {
+		
+
+		if qty<=(p.Quantity ){
+
+			resp := Response{Result: true, Message: "stock is available "}
+			jsonResponse, _ := json.Marshal(resp)
+			w.Write(jsonResponse)
+					//fmt.Println("stock is available ")
+
+		}else{
+
+			resp := Response{Result: false, Message: "Out of Stock Only haves"}
+			jsonResponse, _ := json.Marshal(resp)
+			w.Write(jsonResponse)
+       	//fmt.Println("Out of Stock Only haves ",(p.Quantity ))
+        }
+	}else{
+
+		resp := Response{Result: false, Message: "Out of Stock SORRY"}
+		jsonResponse, _ := json.Marshal(resp)
+	    w.Write(jsonResponse)
+
+		//fmt.Println("Out of Stock SORRY")
+
+	}
+
+}	
